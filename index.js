45a537236524e8b1c971e80d45843a61f5adca89
YogaDwiAndhika <yogadwiandhika10@gmail.com>
2026-05-25 11:59:57 +0700

    update firebase config

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors()); // Enable CORS for all origins
 app.use(bodyParser.json());
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
